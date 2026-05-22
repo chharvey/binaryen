751680ccc28ea0ebe1e9eec82e1d6b25677b80aa
Chris Harvey <1362083+chharvey@users.noreply.github.com>
2026-05-21 23:01:27 -0400

    feat: type the variable instrs

--- ts/src/services/expression-builder/variable.ts
+++ ts/src/services/expression-builder/variable.ts
@@ -12,6 +12,7 @@ import type {
 import type {
 	ExpressionRef,
 	Type,
+	none,
 } from "../../constants.ts";
 
 
@@ -23,21 +24,21 @@ export function local(mod: Module) {
 		 * Creates a `(local.get)` for the local at the specified index.
 		 * Note that we must specify the type here as we may not have created the local being accessed yet.
 		 */
-		get: (index: number, typ: Type): ExpressionRef => (
-			BinaryenObj["_BinaryenLocalGet"](mod[PTR], index, typ) as ExpressionRef
+		get: <T extends ExpressionRef>(index: number, typ: Type): T => (
+			BinaryenObj["_BinaryenLocalGet"](mod[PTR], index, typ) as T
 		),
 
 		/** Creates a `(local.set)` for the local at the specified index. */
-		set: (index: number, value: ExpressionRef): ExpressionRef => (
-			BinaryenObj["_BinaryenLocalSet"](mod[PTR], index, value) as ExpressionRef
+		set: (index: number, value: ExpressionRef): none => (
+			BinaryenObj["_BinaryenLocalSet"](mod[PTR], index, value) as none
 		),
 
 		/**
 		 * Creates a `(local.tee)` for the local at the specified index.
 		 * Note that we must specify the type here as we may not have created the local being accessed yet.
 		 */
-		tee: (index: number, value: ExpressionRef, typ: Type): ExpressionRef => (
-			BinaryenObj["_BinaryenLocalTee"](mod[PTR], index, value, typ) as ExpressionRef
+		tee: <T extends ExpressionRef>(index: number, value: T, typ: Type): T => (
+			BinaryenObj["_BinaryenLocalTee"](mod[PTR], index, value, typ) as T
 		),
 	} as const;
 }
@@ -51,13 +52,13 @@ export function global(mod: Module) {
 		 * Creates a `(global.get)` for the global with the specified name.
 		 * Note that we must specify the type here as we may not have created the global being accessed yet.
 		 */
-		get: (name: string, typ: Type): ExpressionRef => (
-			preserveStack(() => BinaryenObj["_BinaryenGlobalGet"](mod[PTR], strToStack(name), typ) as ExpressionRef)
+		get: <T extends ExpressionRef>(name: string, typ: Type): T => (
+			preserveStack(() => BinaryenObj["_BinaryenGlobalGet"](mod[PTR], strToStack(name), typ) as T)
 		),
 
 		/** Creates a `(global.set)` for the global with the specified name. */
-		set: (name: string, value: ExpressionRef): ExpressionRef => (
-			preserveStack(() => BinaryenObj["_BinaryenGlobalSet"](mod[PTR], strToStack(name), value) as ExpressionRef)
+		set: (name: string, value: ExpressionRef): none => (
+			preserveStack(() => BinaryenObj["_BinaryenGlobalSet"](mod[PTR], strToStack(name), value) as none)
 		),
 	} as const;
 }

d8a6e9085a97e802b386351f890bc2cc1758c470
Chris Harvey <1362083+chharvey@users.noreply.github.com>
2026-05-21 23:46:02 -0400

    feat: type the table instrs

--- ts/src/services/expression-builder/table.ts
+++ ts/src/services/expression-builder/table.ts
@@ -12,6 +12,7 @@ import type {
 import type {
 	ExpressionRef,
 	Type,
+	none,
 } from "../../constants.ts";
 
 
@@ -20,27 +21,27 @@ import type {
 export function table(mod: Module) {
 	return {
 		/** Load an element in a table. */
-		get: (name: string, index: number, typ: Type): ExpressionRef => (
-			preserveStack(() => BinaryenObj["_BinaryenTableGet"](mod[PTR], strToStack(name), index, typ) as ExpressionRef)
+		get: <T extends ExpressionRef>(name: string, index: number, typ: Type): T => (
+			preserveStack(() => BinaryenObj["_BinaryenTableGet"](mod[PTR], strToStack(name), index, typ) as T)
 		),
 
 		/** Store an element in a table. */
-		set: (name: string, index: number, value: ExpressionRef): ExpressionRef => (
-			preserveStack(() => BinaryenObj["_BinaryenTableSet"](mod[PTR], strToStack(name), index, value) as ExpressionRef)
+		set: (name: string, index: number, value: ExpressionRef): none => (
+			preserveStack(() => BinaryenObj["_BinaryenTableSet"](mod[PTR], strToStack(name), index, value) as none)
 		),
 
 		/** Returns the current size of a table. */
-		size: (name: string): ExpressionRef => (
-			preserveStack(() => BinaryenObj["_BinaryenTableSize"](mod[PTR], strToStack(name)) as ExpressionRef)
+		size: <T extends ExpressionRef>(name: string): T => (
+			preserveStack(() => BinaryenObj["_BinaryenTableSize"](mod[PTR], strToStack(name)) as T)
 		),
 
 		/** Grows table by a given delta and returns the previous size, or -1 if not enough space can be allocated. */
-		grow: (name: string, value: ExpressionRef, delta: ExpressionRef): ExpressionRef => (
-			preserveStack(() => BinaryenObj["_BinaryenTableGrow"](mod[PTR], strToStack(name), value, delta) as ExpressionRef)
+		grow: <T extends ExpressionRef>(name: string, value: ExpressionRef, delta: ExpressionRef): T => (
+			preserveStack(() => BinaryenObj["_BinaryenTableGrow"](mod[PTR], strToStack(name), value, delta) as T)
 		),
 
-		// TODO: fill
-		// TODO: copy
-		// TODO: init
+		// TODO: fill // (T, ExpressionRef, T) => none
+		// TODO: copy // (ExpressionRef, ExpressionRef, ExpressionRef) => none
+		// TODO: init // (ExpressionRef, i32, i32) => none
 	} as const;
 }
